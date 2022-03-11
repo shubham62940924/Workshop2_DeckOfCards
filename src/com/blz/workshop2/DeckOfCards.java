2ba7a7e809dd6ec772eb2004c06216472e3d2360
first_name <ss329222@gmail.com>
2022-03-11 16:08:23 +0530

    Added:InitializeGame

--- src/com/blz/workshop2/DeckOfCards.java
+++ src/com/blz/workshop2/DeckOfCards.java
@@ -1,19 +1,15 @@
 package com.blz.workshop2;
 
-/**
- *@Purpose:Setup the initial game with deck of cards and make sure we have unique cards, 
- *The number of cards are 52. Should be able to make unique deck. Maintain the ranks to further compare.
- */
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DeckOfCards {
-	public static ArrayList<String> deckCards = new ArrayList<>();
-	Scanner scanner = new Scanner(System.in);
+	public static final Scanner scanner = new Scanner(System.in);
+	public ArrayList<String> cardsDeck = new ArrayList<String>();
 
-//Welcome message 
+	// Welcome message
 	public void welcome() {
-		System.out.println("Welcome to the Deck of Card DashBoard");
+		System.out.println("Welcome to the gaming world of Deck of Cards");
 	}
 
 	/**
@@ -22,24 +18,25 @@ public class DeckOfCards {
 	 * @Function :For loop to get cards
 	 * @Return :Int string
 	 */
-	public void deckOfCard() {
+	public void deckOfCards() {
 		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
 		int numOfCards = suits.length * ranks.length;
-		System.out.println("\nNumber of cards in the deck:" + numOfCards);
+		System.out.println("\nNumber of cards in the deck is : " + numOfCards);
+		// First we have to itterate for loop for ranks starting from index 0.
 		for (int i = 0; i < ranks.length; i++) {
+			// Now we have to ittreate the suits for all the indexes of ranks
 			for (int j = 0; j < suits.length; j++) {
-				deckCards.add(ranks[i] + "->" + suits[j]);
+				cardsDeck.add(ranks[i] + "----->" + suits[j]);
 			}
 		}
-		toDisplay(deckCards);
+		toDisplay(cardsDeck);
 	}
 
-	/**
-	 * @Purpose :To dispaly arraylist created
-	 * @Param :Cards in Deck
-	 * @Function :For loop ,SOP
-	 * @Return :Card Sequence
+	/*
+	 * @Purpose :To shuffle Cards and display accordingly in consecutive sequence
+	 * 
+	 * @Param :suits , ranks, no of cards
 	 */
 	public static void toDisplay(ArrayList<String> cardsDeck) {
 		System.out.println("\nCards in Deck:");
@@ -49,30 +46,10 @@ public class DeckOfCards {
 		System.out.println();
 	}
 
-	/**
-	 * @Purpose :To get no players
-	 * @Param :Choice i/p
-	 * @Function :condition
-	 * @Return :no return
-	 */
-	public void noOfPlayers() {
-		System.out.print("\nEnter the  number of players minimum 2 , maximum 4 : ");
-		int player = scanner.nextInt();
-		if (player >= 2 && player <= 4) {
-			System.out.println("\n" + player + " players will play the game");
-		} else {
-			System.out.println("Please enter number of players in the Range");
-			this.noOfPlayers();
-			scanner.close();
-		}
-	}
-
-//Main class
+	// Main class
 	public static void main(String[] args) {
 		DeckOfCards deckOfCardsGame = new DeckOfCards();
 		deckOfCardsGame.welcome();
-		deckOfCardsGame.deckOfCard();
-		deckOfCardsGame.noOfPlayers();
-
+		deckOfCardsGame.deckOfCards();
 	}
 }
